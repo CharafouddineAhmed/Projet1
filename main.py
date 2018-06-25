e28473ebed0ccd5ad976fd8df3c4b2e0ddc73c0f
charafouddine01 <charafouddine01@gmail.fr>
2018-06-25 22:39:46 +0200

    indexatoin des données dans Elasticsearch

--- main.py
+++ main.py
@@ -3,29 +3,34 @@
 
 import sys, os, json, requests
 
-FILE = ""
+from datetime import datetime
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
 
 # lecture du fichier et affichage en json
 try:
 
-    # Creation de l'objet Json
-    data = {}
-
     #Lecture de fichier
     fichier = open("exemple.log", "r")
+    i = 1
     for line in fichier:
         donnee = line.split(" ")
-        data['0'] = donnee[0]
-        data['1'] = donnee[1]
-        data['2'] = donnee[2]
-        data['3'] = donnee[3]
-        data['4'] = donnee[4]
-        data['5'] = donnee[5]
-        data['6'] = donnee[6]
-        data['7'] = donnee[7]
-        data['8'] = donnee[8]
-        data['9'] = donnee[9]
+        data = {
+            '0' : donnee[0],
+            '1' : donnee[1],
+            '2' : donnee[2],
+            '3' : donnee[3],
+            '4' : donnee[4],
+            '5' : donnee[5],
+            '6' : donnee[6],
+            '7' : donnee[7],
+            '8' : donnee[8],
+            '9' : donnee[9],
+        }
 
+        res = es.index(index="test-index-2", doc_type='tweet', id = i, body=data)
+        print(res['result'])
+        i = i + 1
         # Creation du fichier json json_data
         json_data = json.dumps(data, indent=4, sort_keys=True)
 
